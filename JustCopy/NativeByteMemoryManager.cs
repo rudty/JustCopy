83683f23f8b075f3097e3edd5f8ef477c1af72cb
rudty <rudtyz@gmail.com>
2024-05-29 22:29:43 +0900

    NativeByteMemoryManager

---
+++ JustCopy/NativeByteMemoryManager.cs
@@ -0,0 +1,67 @@
+﻿namespace JustCopy
+{
+    using System;
+    using System.Buffers;
+    using System.Threading;
+
+    public sealed unsafe class NativeByteMemoryManager : MemoryManager<byte>
+    {
+        private int referenceCount = 1;
+        private readonly byte* pointer;
+        public int Count { get; }
+
+        public int ReferenceCount => referenceCount;
+
+        public override Memory<byte> Memory { get; }
+
+        public NativeByteMemoryManager(int size)
+        {
+            Count = size;
+            Memory = CreateMemory(size);
+            pointer = Alloc(size);
+        }
+
+#if NET6_0_OR_GREATER
+        private static byte* Alloc(int size)
+        {
+            return (byte*)System.Runtime.InteropServices.NativeMemory.Alloc((nuint)size);
+        }
+
+        private static void Free(byte* b)
+        {
+            System.Runtime.InteropServices.NativeMemory.Free(b);
+        }
+#else
+        private static byte* Alloc(int size)
+        {
+            return (byte*)System.Runtime.InteropServices.Marshal.AllocHGlobal(size);
+        }
+
+        private static void Free(byte* b)
+        {
+            System.Runtime.InteropServices.Marshal.FreeHGlobal((IntPtr)b);
+        }
+#endif
+
+        public override Span<byte> GetSpan() => new Span<byte>(pointer, Count);
+
+        public override MemoryHandle Pin(int elementIndex = 0)
+        {
+            Interlocked.Increment(ref referenceCount);
+            return new MemoryHandle(pointer + elementIndex, default, this);
+        }
+
+        public override void Unpin()
+        {
+            if (Interlocked.Decrement(ref referenceCount) == 0)
+            {
+                Free(pointer);
+            }
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            Unpin();
+        }
+    }
+}
---
+++ Test/NativeByteMemoryManagerTest.cs
@@ -0,0 +1,59 @@
+﻿namespace Test;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using JustCopy;
+
+public unsafe class NativeByteMemoryManagerTest
+{
+    [Fact]
+    public void PinTest()
+    {
+        var m = new NativeByteMemoryManager(1024);
+        Assert.Equal(1,m.ReferenceCount);
+        var memoryHandle = m.Pin();
+        Assert.Equal(2, m.ReferenceCount);
+        var b = (byte*)memoryHandle.Pointer;
+
+        for (var i = 0; i < 1024; ++i)
+        {
+            b[i] = (byte)i;
+        }
+
+        m.Unpin();
+        Assert.Equal(1, m.ReferenceCount);
+
+        IDisposable d = m;
+        d.Dispose();
+        Assert.Equal(0, m.ReferenceCount);
+    }
+
+    [Fact]
+    public void SpanTest()
+    {
+        var m = new NativeByteMemoryManager(1024);
+        var span = m.GetSpan();
+        Assert.Equal(1024, span.Length);
+        for (var i = 0; i < 1024; ++i)
+        {
+            span[i] = (byte)i;
+        }
+    }
+
+    [Fact]
+    public void MemoryTest()
+    {
+        var m = new NativeByteMemoryManager(1024);
+        var memory = m.Memory;
+        Assert.Equal(1024, memory.Length);
+        var span = memory.Span;
+        Assert.Equal(1024, span.Length);
+        for (var i = 0; i < 1024; ++i)
+        {
+            span[i] = (byte)i;
+        }
+    }
+}
